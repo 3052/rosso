26aca2c06beaeaeb740dedf19a99876aeba97538
27 <27@riseup.net>
2026-03-21 16:23:41 -0500

    +// WE COULD CACHE THE APP SECRET BUT THEN TO CHANGE LOCATION YOU WOULD NEED TO

--- examples/paramount/paramount.go
+++ examples/paramount/paramount.go
@@ -1,3 +1,5 @@
+// WE COULD CACHE THE APP SECRET BUT THEN TO CHANGE LOCATION YOU WOULD NEED TO
+// SET PROXY AND APP SECRET
 package main
 
 import (
@@ -8,8 +10,109 @@ import (
    "net/http"
 )
 
+func (c *client) do_username_password() error {
+   app_secret, err := paramount.FetchAppSecret()
+   if err != nil {
+      return err
+   }
+   at, err := paramount.GetAt(app_secret)
+   if err != nil {
+      return err
+   }
+   c.CbsCom, err = paramount.Login(at, c.username, c.password)
+   if err != nil {
+      return err
+   }
+   return cache.Write(c)
+}
+
+func main() {
+   log.SetFlags(log.Ltime)
+   err := new(client).do()
+   if err != nil {
+      log.Fatal(err)
+   }
+}
+
+var cache maya.Cache
+
+type client struct {
+   CbsCom    *http.Cookie
+   Dash      *paramount.Dash
+   //--------------------
+   Job maya.Job
+   //--------------------
+   Proxy string
+   //--------------------
+   username string
+   password string
+   //--------------------
+   ParamountId string
+   //--------------------
+   dash_id    string
+   get_cookie bool
+}
+
+func (c *client) do() error {
+   err := cache.Setup("rosso/paramount.xml")
+   if err != nil {
+      return err
+   }
+   with_cache := cache.Read(c)
+   playReady := maya.StringVar(&c.Job.PlayReady, "PR", "PlayReady")
+   //--------------------------------------------------------------
+   threads := maya.IntVar(&c.Job.Threads, "t", "threads")
+   //--------------------------------------------------------------
+   proxy := maya.StringVar(&c.Proxy, "x", "proxy")
+   //--------------------------------------------------------------
+   username := maya.StringVar(&c.username, "U", "username")
+   password := maya.StringVar(&c.password, "P", "password")
+   //--------------------------------------------------------------
+   paramount_id := maya.StringVar(&c.ParamountId, "p", "paramount ID")
+   get_cookie := maya.BoolVar(&c.get_cookie, "c", "get cookie")
+   //--------------------------------------------------------------
+   dash_id := maya.StringVar(&c.dash_id, "d", "DASH ID")
+   set := maya.Parse()
+   err = maya.SetProxy(c.Proxy, "*.m4s,*.mp4")
+   if err != nil {
+      return err
+   }
+   if set[playReady] {
+      return cache.Write(c)
+   }
+   if set[threads] {
+      return cache.Write(c)
+   }
+   if set[proxy] {
+      return cache.Write(c)
+   }
+   if set[username] {
+      if set[password] {
+         return with_cache(c.do_username_password)
+      }
+   }
+   if set[paramount_id] {
+      return with_cache(c.do_paramount)
+   }
+   if set[dash_id] {
+      return with_cache(c.do_dash_id)
+   }
+   return maya.Usage([][]*flag.Flag{
+      {playReady},
+      {threads},
+      {proxy},
+      {username, password},
+      {paramount_id, get_cookie},
+      {dash_id, get_cookie},
+   })
+}
+
 func (c *client) do_dash_id() error {
-   at, err := paramount.GetAt(c.AppSecret)
+   app_secret, err := paramount.FetchAppSecret()
+   if err != nil {
+      return err
+   }
+   at, err := paramount.GetAt(app_secret)
    if err != nil {
       return err
    }
@@ -25,7 +128,11 @@ func (c *client) do_dash_id() error {
 }
 
 func (c *client) do_paramount() error {
-   at, err := paramount.GetAt(c.AppSecret)
+   app_secret, err := paramount.FetchAppSecret()
+   if err != nil {
+      return err
+   }
+   at, err := paramount.GetAt(app_secret)
    if err != nil {
       return err
    }
@@ -46,102 +153,4 @@ func (c *client) do_paramount() error {
       return err
    }
    return maya.ListDash(c.Dash.Body, c.Dash.Url)
-}
-
-var cache maya.Cache
-
-func main() {
-   log.SetFlags(log.Ltime)
-   maya.SetProxy("", "*.m4s,*.mp4")
-   err := new(client).do()
-   if err != nil {
-      log.Fatal(err)
-   }
-}
-
-type client struct {
-   AppSecret string
-   CbsCom    *http.Cookie
-   Dash      *paramount.Dash
-   //--------------------
-   Job maya.Job
-   //--------------------
-   username string
-   password string
-   //--------------------
-   ParamountId string
-   //--------------------
-   dash_id    string
-   get_cookie bool
-}
-
-func (c *client) do_app_secret() error {
-   var err error
-   c.AppSecret, err = paramount.FetchAppSecret()
-   if err != nil {
-      return err
-   }
-   return cache.Write(c)
-}
-
-func (c *client) do_username_password() error {
-   at, err := paramount.GetAt(c.AppSecret)
-   if err != nil {
-      return err
-   }
-   c.CbsCom, err = paramount.Login(at, c.username, c.password)
-   if err != nil {
-      return err
-   }
-   return cache.Write(c)
-}
-
-func (c *client) do() error {
-   err := cache.Setup("rosso/paramount.xml")
-   if err != nil {
-      return err
-   }
-   with_cache := cache.Read(c)
-   playReady := maya.StringVar(&c.Job.PlayReady, "PR", "PlayReady")
-   //--------------------------------------------------------------
-   threads := maya.IntVar(&c.Job.Threads, "t", "threads")
-   //--------------------------------------------------------------
-   app_secret := maya.BoolVar(new(bool), "a", "app secret")
-   //--------------------------------------------------------------
-   username := maya.StringVar(&c.username, "U", "username")
-   password := maya.StringVar(&c.password, "P", "password")
-   //--------------------------------------------------------------
-   paramount_id := maya.StringVar(&c.ParamountId, "p", "paramount ID")
-   get_cookie := maya.BoolVar(&c.get_cookie, "c", "get cookie")
-   //--------------------------------------------------------------
-   dash_id := maya.StringVar(&c.dash_id, "d", "DASH ID")
-   set := maya.Parse()
-   if set[playReady] {
-      return cache.Write(c)
-   }
-   if set[threads] {
-      return cache.Write(c)
-   }
-   if set[app_secret] {
-      return c.do_app_secret()
-   }
-   if set[username] {
-      if set[password] {
-         return with_cache(c.do_username_password)
-      }
-   }
-   if set[paramount_id] {
-      return with_cache(c.do_paramount)
-   }
-   if set[dash_id] {
-      return with_cache(c.do_dash_id)
-   }
-   return maya.Usage([][]*flag.Flag{
-      {playReady},
-      {threads},
-      {app_secret},
-      {username, password},
-      {paramount_id, get_cookie},
-      {dash_id, get_cookie},
-   })
 }
--- paramount/paramount.go
+++ paramount/paramount.go
@@ -17,6 +17,52 @@ import (
    "strings"
 )
 
+// WARNING IF YOU RUN THIS TOO MANY TIMES YOU WILL GET AN IP BAN. HOWEVER THE BAN
+// IS ONLY FOR THE ANDROID CLIENT NOT WEB CLIENT
+func Login(at, username, password string) (*http.Cookie, error) {
+   data := url.Values{
+      "j_username": {username},
+      "j_password": {password},
+   }.Encode()
+   var req http.Request
+   req.Method = "POST"
+   req.URL = &url.URL{
+      Scheme:   "https",
+      Host:     "www.paramountplus.com",
+      Path:     "/apps-api/v2.0/androidphone/auth/login.json",
+      RawQuery: url.Values{"at": {at}}.Encode(),
+   }
+   req.Header = http.Header{}
+   req.Header.Set("content-type", "application/x-www-form-urlencoded")
+   // randomly fails if this is missing
+   req.Header.Set("user-agent", "!")
+   req.Body = io.NopCloser(strings.NewReader(data))
+   resp, err := http.DefaultClient.Do(&req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   if resp.StatusCode != http.StatusOK {
+      return nil, errors.New(resp.Status)
+   }
+   var result struct {
+      Message string
+   }
+   err = json.NewDecoder(resp.Body).Decode(&result)
+   if err != nil {
+      return nil, err
+   }
+   if result.Message != "" {
+      return nil, errors.New(result.Message)
+   }
+   for _, cookie := range resp.Cookies() {
+      if cookie.Name == "CBS_COM" {
+         return cookie, nil
+      }
+   }
+   return nil, http.ErrNoCookie
+}
+
 func GetAt(appSecret string) (string, error) {
    // 1. Decode hex secret key
    key, err := hex.DecodeString(secret_key)
@@ -77,49 +123,6 @@ func FetchAppSecret() (string, error) {
       return AppSecrets[0].International, nil
    }
    return "", errors.New("unexpected or missing server header value")
-}
-
-// WARNING IF YOU RUN THIS TOO MANY TIMES YOU WILL GET AN IP BAN. HOWEVER THE BAN
-// IS ONLY FOR THE ANDROID CLIENT NOT WEB CLIENT
-func Login(at, username, password string) (*http.Cookie, error) {
-   data := url.Values{
-      "j_username": {username},
-      "j_password": {password},
-   }.Encode()
-   var req http.Request
-   req.Method = "POST"
-   req.URL = &url.URL{
-      Scheme:   "https",
-      Host:     "www.paramountplus.com",
-      Path:     "/apps-api/v2.0/androidphone/auth/login.json",
-      RawQuery: url.Values{"at": {at}}.Encode(),
-   }
-   req.Header = http.Header{}
-   req.Header.Set("content-type", "application/x-www-form-urlencoded")
-   // randomly fails if this is missing
-   req.Header.Set("user-agent", "!")
-   req.Body = io.NopCloser(strings.NewReader(data))
-   resp, err := http.DefaultClient.Do(&req)
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-   var result struct {
-      Message string
-   }
-   err = json.NewDecoder(resp.Body).Decode(&result)
-   if err != nil {
-      return nil, err
-   }
-   if result.Message != "" {
-      return nil, errors.New(result.Message)
-   }
-   for _, cookie := range resp.Cookies() {
-      if cookie.Name == "CBS_COM" {
-         return cookie, nil
-      }
-   }
-   return nil, http.ErrNoCookie
 }
 
 type Dash struct {
