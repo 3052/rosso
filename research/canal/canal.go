6acd155e663ce7589a759e9dd3e2b132707ca28b
27 <27@riseup.net>
2026-04-06 22:25:31 -0500

    deleted:    research/canal/5_login_init.go

--- research/canal/canal.go
+++ research/canal/canal.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+   "bytes"
    "crypto/hmac"
    "crypto/sha256"
    "encoding/base64"
+   "encoding/json"
+   "fmt"
    "io"
    "log"
    "net/http"
@@ -12,6 +15,141 @@ import (
    "strings"
    "time"
 )
+
+func (a *App) LoginSubmit(username, password string) error {
+   u, err := url.Parse("https://m7cp.login.solocoo.tv/login")
+   if err != nil {
+      return err
+   }
+   payload := LoginSubmitPayload{
+      Ticket: a.Ticket,
+      UserInput: UserInput{
+         Username: username,
+         Password: password,
+      },
+   }
+
+   body, err := json.Marshal(payload)
+   if err != nil {
+      return err
+   }
+
+   authHeader, err := get_client(u, body)
+   if err != nil {
+      return err
+   }
+
+   req, err := http.NewRequest("POST", u.String(), bytes.NewBuffer(body))
+   if err != nil {
+      return err
+   }
+   setCommonHeaders(req)
+   req.Header.Set("Content-Type", "application/json")
+   req.Header.Set("Authorization", authHeader)
+
+   resp, err := a.Client.Do(req)
+   if err != nil {
+      return err
+   }
+   defer resp.Body.Close()
+
+   if resp.StatusCode != 200 {
+      return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+   }
+
+   var result struct {
+      Label    string `json:"label"`
+      Result   string `json:"result"`
+      SsoToken string `json:"ssoToken"`
+   }
+   if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+      return err
+   }
+
+   if result.Result == "success" {
+      log.Println("Login successful! Acquired final SSO token.")
+      a.SsoToken = result.SsoToken
+   } else {
+      log.Printf("Login response label: %s", result.Label)
+   }
+
+   return nil
+}
+
+type UserInput struct {
+   Username string `json:"username"`
+   Password string `json:"password"`
+}
+
+type LoginSubmitPayload struct {
+   Ticket    string    `json:"ticket"`
+   UserInput UserInput `json:"userInput"`
+}
+
+func (a *App) LoginInit() error {
+   u, err := url.Parse("https://m7cp.login.solocoo.tv/login")
+   if err != nil {
+      return err
+   }
+   payload := LoginInitPayload{
+      DeviceInfo: DeviceInfo{
+         OsVersion:        "Windows 10",
+         DeviceModel:      "Firefox",
+         DeviceType:       "PC",
+         DeviceSerial:     a.DeviceSerial,
+         DeviceOem:        "Firefox",
+         DevicePrettyName: "Firefox 140.0",
+         AppVersion:       "12.9",
+         Language:         "en_US",
+         Brand:            "m7cp",
+         Country:          "CZ",
+      },
+   }
+
+   body, err := json.Marshal(payload)
+   if err != nil {
+      return err
+   }
+
+   authHeader, err := get_client(u, body)
+   if err != nil {
+      return err
+   }
+
+   req, err := http.NewRequest("POST", u.String(), bytes.NewBuffer(body))
+   if err != nil {
+      return err
+   }
+   setCommonHeaders(req)
+   req.Header.Set("Content-Type", "application/json")
+   req.Header.Set("Authorization", authHeader)
+
+   resp, err := a.Client.Do(req)
+   if err != nil {
+      return err
+   }
+   defer resp.Body.Close()
+
+   if resp.StatusCode != 200 {
+      return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+   }
+
+   var result struct {
+      Ticket string `json:"ticket"`
+   }
+   if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+      return err
+   }
+
+   a.Ticket = result.Ticket
+   return nil
+}
+
+type LoginInitPayload struct {
+   ProvisionData string     `json:"provisionData"`
+   DeviceInfo    DeviceInfo `json:"deviceInfo"`
+   OldSsoToken   string     `json:"oldSsoToken"`
+}
 
 type DeviceInfo struct {
    OsVersion        string `json:"osVersion"`
