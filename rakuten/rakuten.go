1d5770d52d1e8aebaf725f32131351f3249e390d
27 <27@riseup.net>
2026-04-13 19:07:07 -0500

    deleted:    rakuten/models.go

--- rakuten/rakuten.go
+++ rakuten/rakuten.go
@@ -11,29 +11,154 @@ import (
    "strings"
 )
 
-func (s Stream) Dash() (*Dash, error) {
-   resp, err := http.Get(s.StreamInfos[0].Url)
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-   body, err := io.ReadAll(resp.Body)
-   if err != nil {
-      return nil, err
-   }
-   return &Dash{Body: body, Url: resp.Request.URL}, nil
+func (s *StreamInfo) ParseDash() (*url.URL, error) {
+   return url.Parse(s.Url)
 }
 
-func (s Stream) Widevine(data []byte) ([]byte, error) {
-   resp, err := http.Post(
-      s.StreamInfos[0].LicenseUrl, "application/x-protobuf",
-      bytes.NewReader(data),
-   )
+type StreamInfo struct {
+   LicenseUrl string `json:"license_url"`
+   Url        string `json:"url"`
+}
+
+var classificationMap = map[string]int{
+   "cz": 272,
+   "es": 5,
+   "fr": 23,
+   "ie": 41,
+   "nl": 69,
+   "pl": 277,
+   "pt": 64,
+   "se": 282,
+   "uk": 18,
+}
+
+// Parse extracts metadata from a Rakuten URL and returns a new Content struct
+func ParseContent(urlData string) (*Content, error) {
+   urlParse, err := url.Parse(urlData)
    if err != nil {
       return nil, err
    }
-   defer resp.Body.Close()
-   return io.ReadAll(resp.Body)
+
+   c := &Content{}
+
+   // Trim prefix once and extract the market code
+   path := strings.TrimPrefix(urlParse.Path, "/")
+   c.MarketCode, _, _ = strings.Cut(path, "/")
+
+   // Check if the market code exists in the map and set ClassificationId
+   var ok bool
+   c.ClassificationId, ok = classificationMap[c.MarketCode]
+   if !ok {
+      return nil, errors.New("unknown market code")
+   }
+
+   // 1. Check Query Parameters
+   query := urlParse.Query()
+   contentType := query.Get("content_type")
+   switch contentType {
+   case "movies":
+      c.Id = query.Get("content_id")
+      c.Type = contentType
+      return c, nil
+   case "tv_shows":
+      c.Id = query.Get("tv_show_id")
+      c.Type = contentType
+      return c, nil
+   }
+
+   // 2. Check Path Segments
+   segments := strings.Split(path, "/")
+   for _, segment := range segments {
+      switch segment {
+      case "movies", "tv_shows":
+         c.Id = segments[len(segments)-1]
+         c.Type = segment
+         return c, nil
+      }
+   }
+
+   return nil, errors.New("not a movie or tv show url")
+}
+
+// String implementation for MovieOrEpisode to pretty print details
+func (m *MovieOrEpisode) String() string {
+   seen := make(map[string]bool)
+   var data strings.Builder
+   data.WriteString("title = ")
+   data.WriteString(m.Title)
+   data.WriteString("\nid = ")
+   data.WriteString(m.Id)
+   for _, streamData := range m.ViewOptions.Private.Streams {
+      for _, language := range streamData.AudioLanguages {
+         if !seen[language.Id] {
+            seen[language.Id] = true
+            data.WriteString("\naudio language = ")
+            data.WriteString(language.Id)
+         }
+      }
+   }
+   return data.String()
+}
+
+func (t TvShow) String() string {
+   var data strings.Builder
+   for i, season := range t.Seasons {
+      if i >= 1 {
+         data.WriteByte('\n')
+      }
+      data.WriteString("season id = ")
+      data.WriteString(season.Id)
+   }
+   return data.String()
+}
+
+// Content represents the parsed Rakuten URL data
+type Content struct {
+   Id               string
+   MarketCode       string
+   Type             string
+   ClassificationId int
+}
+
+// Constants for device and player configuration
+const DeviceId = "atvui40"
+
+const (
+   PlayReady Player = DeviceId + ":DASH-CENC:PR"
+   Widevine  Player = DeviceId + ":DASH-CENC:WVM"
+)
+
+const (
+   Fhd VideoQuality = "FHD"
+   Hd  VideoQuality = "HD"
+)
+
+type VideoQuality string
+
+type Player string
+
+type Season struct {
+   Episodes []MovieOrEpisode `json:"episodes"`
+}
+
+type TvShow struct {
+   Seasons []struct {
+      Id string `json:"id"`
+   } `json:"seasons"`
+}
+
+type MovieOrEpisode struct {
+   Title       string `json:"title"`
+   Id          string `json:"id"`
+   ViewOptions struct {
+      Private struct {
+         Streams []struct {
+            AudioLanguages []struct {
+               Id string `json:"id"`
+            } `json:"audio_languages"`
+         } `json:"streams"`
+      } `json:"private"`
+   } `json:"view_options"`
 }
 
 func (c *Content) IsMovie() bool {
@@ -138,70 +263,9 @@ func (c *Content) Movie() (*MovieOrEpisode, error) {
    return &result.Data, nil
 }
 
-var classificationMap = map[string]int{
-   "cz": 272,
-   "es": 5,
-   "fr": 23,
-   "ie": 41,
-   "nl": 69,
-   "pl": 277,
-   "pt": 64,
-   "se": 282,
-   "uk": 18,
-}
-
-// Parse extracts metadata from a Rakuten URL and returns a new Content struct
-func ParseContent(urlData string) (*Content, error) {
-   urlParse, err := url.Parse(urlData)
-   if err != nil {
-      return nil, err
-   }
-
-   c := &Content{}
-
-   // Trim prefix once and extract the market code
-   path := strings.TrimPrefix(urlParse.Path, "/")
-   c.MarketCode, _, _ = strings.Cut(path, "/")
-
-   // Check if the market code exists in the map and set ClassificationId
-   var ok bool
-   c.ClassificationId, ok = classificationMap[c.MarketCode]
-   if !ok {
-      return nil, errors.New("unknown market code")
-   }
-
-   // 1. Check Query Parameters
-   query := urlParse.Query()
-   contentType := query.Get("content_type")
-   switch contentType {
-   case "movies":
-      c.Id = query.Get("content_id")
-      c.Type = contentType
-      return c, nil
-   case "tv_shows":
-      c.Id = query.Get("tv_show_id")
-      c.Type = contentType
-      return c, nil
-   }
-
-   // 2. Check Path Segments
-   segments := strings.Split(path, "/")
-   for _, segment := range segments {
-      switch segment {
-      case "movies", "tv_shows":
-         c.Id = segments[len(segments)-1]
-         c.Type = segment
-         return c, nil
-      }
-   }
-
-   return nil, errors.New("not a movie or tv show url")
-}
-
-// Stream requests a playback stream.
 // For TV Shows, 'id' should be the Episode ID.
 // For Movies, 'id' is ignored (uses c.Id).
-func (c *Content) Stream(id, audioLanguage string, playerData Player, quality VideoQuality) (*Stream, error) {
+func (c *Content) FetchStreamInfo(id, audioLanguage string, playerData Player, quality VideoQuality) (*StreamInfo, error) {
    body := map[string]string{
       "audio_language":              audioLanguage,
       "audio_quality":               "2.0",
@@ -234,61 +298,24 @@ func (c *Content) Stream(id, audioLanguage string, playerData Player, quality Vi
    }
    defer resp.Body.Close()
    var result struct {
-      Data   Stream
-      Errors []Error
+      Data struct {
+         StreamInfos []StreamInfo `json:"stream_infos"`
+      }
    }
    err = json.NewDecoder(resp.Body).Decode(&result)
    if err != nil {
       return nil, err
    }
-   if len(result.Errors) >= 1 {
-      return nil, &result.Errors[0]
+   return &result.Data.StreamInfos[0], nil
+}
+
+func (s *StreamInfo) FetchWidevine(data []byte) ([]byte, error) {
+   resp, err := http.Post(
+      s.LicenseUrl, "application/x-protobuf", bytes.NewReader(data),
+   )
+   if err != nil {
+      return nil, err
    }
-   return &result.Data, nil
-}
-
-func (e *Error) Error() string {
-   var data strings.Builder
-   data.WriteString("code = ")
-   data.WriteString(e.Code)
-   data.WriteString("\nmessage = ")
-   data.WriteString(e.Message)
-   return data.String()
-}
-
-type Error struct {
-   Code    string
-   Message string
-}
-
-// String implementation for MovieOrEpisode to pretty print details
-func (m *MovieOrEpisode) String() string {
-   seen := make(map[string]bool)
-   var data strings.Builder
-   data.WriteString("title = ")
-   data.WriteString(m.Title)
-   data.WriteString("\nid = ")
-   data.WriteString(m.Id)
-   for _, streamData := range m.ViewOptions.Private.Streams {
-      for _, language := range streamData.AudioLanguages {
-         if !seen[language.Id] {
-            seen[language.Id] = true
-            data.WriteString("\naudio language = ")
-            data.WriteString(language.Id)
-         }
-      }
-   }
-   return data.String()
-}
-
-func (t TvShow) String() string {
-   var data strings.Builder
-   for i, season := range t.Seasons {
-      if i >= 1 {
-         data.WriteByte('\n')
-      }
-      data.WriteString("season id = ")
-      data.WriteString(season.Id)
-   }
-   return data.String()
+   defer resp.Body.Close()
+   return io.ReadAll(resp.Body)
 }
