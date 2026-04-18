e81fe9b314e1ced62d6976b5c2ae7ba93c879354
27 <27@riseup.net>
2026-04-18 12:31:41 -0500

    +   resp, err := maya.Get(

--- rtbf/maya.go
+++ rtbf/maya.go
@@ -1,7 +1,7 @@
 package rtbf
 
 import (
-   "bytes"
+   "41.neocities.org/maya"
    "encoding/json"
    "errors"
    "io"
@@ -9,25 +9,119 @@ import (
    "net/url"
 )
 
-func (e *Entitlement) FetchWidevine(data []byte) ([]byte, error) {
-   req, err := http.NewRequest(
-      "POST", "https://exposure.api.redbee.live", bytes.NewReader(data),
+func (a *Account) Identity() (*Identity, error) {
+   resp, err := http.PostForm(
+      "https://login.auvio.rtbf.be/accounts.getJWT", url.Values{
+         "APIKey":      {api_key},
+         "login_token": {a.SessionInfo.CookieValue},
+      },
    )
    if err != nil {
       return nil, err
    }
-   req.URL.Path = "/v2/license/customer/RTBF/businessunit/Auvio/widevine"
-   req.URL.RawQuery = url.Values{
-      "contentId":  {e.AssetId},
-      "ls_session": {e.PlayToken},
-   }.Encode()
-   req.Header.Set("content-type", "application/x-protobuf")
-   resp, err := http.DefaultClient.Do(req)
+   defer resp.Body.Close()
+   var result Identity
+   err = json.NewDecoder(resp.Body).Decode(&result)
+   if err != nil {
+      return nil, err
+   }
+   if result.ErrorMessage != "" {
+      return nil, errors.New(result.ErrorMessage)
+   }
+   return &result, nil
+}
+
+func FetchAssetId(path string) (string, error) {
+   resp, err := maya.Get(
+      &url.URL{
+         Scheme: "https",
+         Host:   "bff-service.rtbf.be",
+         Path:   "/auvio/v1.23/pages" + path,
+      },
+      nil,
+   )
+   if err != nil {
+      return "", err
+   }
+   defer resp.Body.Close()
+   if resp.StatusCode != http.StatusOK {
+      return "", errors.New(resp.Status)
+   }
+   var page struct {
+      Data struct {
+         Content struct {
+            AssetId string
+            Media   *struct {
+               AssetId string
+            }
+         }
+      }
+   }
+   err = json.NewDecoder(resp.Body).Decode(&page)
+   if err != nil {
+      return "", err
+   }
+   content := page.Data.Content
+   if content.AssetId != "" {
+      return content.AssetId, nil
+   }
+   if content.Media != nil {
+      return content.Media.AssetId, nil
+   }
+   return "", errors.New("assetId not found")
+}
+
+func (i *Identity) Session() (*Session, error) {
+   body, err := json.Marshal(map[string]any{
+      "device": map[string]string{
+         "deviceId": "",
+         "type":     "WEB",
+      },
+      "jwt": i.IdToken,
+   })
+   if err != nil {
+      return nil, err
+   }
+   resp, err := maya.Post(
+      &url.URL{
+         Scheme: "https",
+         Host:   "exposure.api.redbee.live",
+         Path:   "/v2/customer/RTBF/businessunit/Auvio/auth/gigyaLogin",
+      },
+      map[string]string{"content-type": "application/json"},
+      body,
+   )
    if err != nil {
       return nil, err
    }
    defer resp.Body.Close()
-   if resp.StatusCode != http.StatusOK {
+   result := &Session{}
+   err = json.NewDecoder(resp.Body).Decode(result)
+   if err != nil {
+      return nil, err
+   }
+   return result, nil
+}
+
+func (e *Entitlement) FetchWidevine(body []byte) ([]byte, error) {
+   resp, err := maya.Post(
+      &url.URL{
+         Scheme: "https",
+         Host:   "exposure.api.redbee.live",
+         Path:   "/v2/license/customer/RTBF/businessunit/Auvio/widevine",
+         RawQuery: url.Values{
+            "contentId":  {e.AssetId},
+            "ls_session": {e.PlayToken},
+         }.Encode(),
+      },
+      map[string]string{"content-type": "application/x-protobuf"},
+      body,
+   )
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   if resp.StatusCode != 200 {
       var value struct {
          Message string
       }
--- rtbf/rtbf.go
+++ rtbf/rtbf.go
@@ -1,45 +1,12 @@
 package rtbf
 
 import (
-   "bytes"
    "encoding/json"
    "errors"
    "fmt"
    "net/http"
    "net/url"
 )
-
-func (i *Identity) Session() (*Session, error) {
-   data, err := json.Marshal(map[string]any{
-      "device": map[string]string{
-         "deviceId": "",
-         "type":     "WEB",
-      },
-      "jwt": i.IdToken,
-   })
-   if err != nil {
-      return nil, err
-   }
-   req, err := http.NewRequest(
-      "POST", "https://exposure.api.redbee.live", bytes.NewReader(data),
-   )
-   if err != nil {
-      return nil, err
-   }
-   req.URL.Path = "/v2/customer/RTBF/businessunit/Auvio/auth/gigyaLogin"
-   req.Header.Set("content-type", "application/json")
-   resp, err := http.DefaultClient.Do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-   result := &Session{}
-   err = json.NewDecoder(resp.Body).Decode(result)
-   if err != nil {
-      return nil, err
-   }
-   return result, nil
-}
 
 type Identity struct {
    ErrorMessage string
@@ -52,63 +19,6 @@ type Session struct {
 
 // hard coded in JavaScript
 const api_key = "4_Ml_fJ47GnBAW6FrPzMxh0w"
-
-func FetchAssetId(path string) (string, error) {
-   resp, err := http.Get(
-      "https://bff-service.rtbf.be/auvio/v1.23/pages" + path,
-   )
-   if err != nil {
-      return "", err
-   }
-   defer resp.Body.Close()
-   if resp.StatusCode != http.StatusOK {
-      return "", errors.New(resp.Status)
-   }
-   var page struct {
-      Data struct {
-         Content struct {
-            AssetId string
-            Media   *struct {
-               AssetId string
-            }
-         }
-      }
-   }
-   err = json.NewDecoder(resp.Body).Decode(&page)
-   if err != nil {
-      return "", err
-   }
-   content := page.Data.Content
-   if content.AssetId != "" {
-      return content.AssetId, nil
-   }
-   if content.Media != nil {
-      return content.Media.AssetId, nil
-   }
-   return "", errors.New("assetId not found")
-}
-
-func (a *Account) Identity() (*Identity, error) {
-   resp, err := http.PostForm(
-      "https://login.auvio.rtbf.be/accounts.getJWT", url.Values{
-         "APIKey":      {api_key},
-         "login_token": {a.SessionInfo.CookieValue},
-      },
-   )
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-   var result Identity
-   err = json.NewDecoder(resp.Body).Decode(&result)
-   if err != nil {
-      return nil, err
-   }
-   if result.ErrorMessage != "" {
-      return nil, errors.New(result.ErrorMessage)
-   }
-   return &result, nil
-}
 
 func FetchAccount(id, password string) (*Account, error) {
    resp, err := http.PostForm(
