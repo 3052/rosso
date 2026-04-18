9458af94115000ec34358608f30853ea9f8e9390
27 <27@riseup.net>
2026-04-17 21:23:34 -0500

    new file:   amc/playback.json

--- amc/maya.go
+++ amc/maya.go
@@ -2,54 +2,122 @@ package amc
 
 import (
    "41.neocities.org/maya"
-   "bytes"
+   _ "embed"
    "encoding/json"
    "fmt"
-   "net/http"
    "net/url"
 )
 
-func Playback(authToken string, videoID int) (*PlaybackResult, error) {
-   url := fmt.Sprintf("https://gw.cds.amcn.com/playback-id/api/v1/playback/%d", videoID)
-
-   payload := []byte(`{"adtags":{"lat":0,"mode":"on-demand","playerHeight":0,"playerWidth":0,"ppid":0,"url":"-"}}`)
-
-   req, err := http.NewRequest(http.MethodPost, url, bytes.NewReader(payload))
-   if err != nil {
-      return nil, err
-   }
-
-   req.Header.Set("authorization", "Bearer "+authToken)
-   req.Header.Set("content-type", "application/json")
-   req.Header.Set("x-amcn-language", "en")
-   req.Header.Set("x-amcn-network", "amcplus")
-   req.Header.Set("x-amcn-platform", "web")
-   req.Header.Set("x-amcn-service-id", "amcplus")
-   req.Header.Set("x-amcn-tenant", "amcn")
-   req.Header.Set("x-amcn-device-ad-id", "-")
-   req.Header.Set("x-ccpa-do-not-sell", "doNotPassData")
-
-   resp, err := http.DefaultClient.Do(req)
+func Unauth() (*AuthData, error) {
+   resp, err := maya.Post(
+      &url.URL{
+         Scheme: "https",
+         Host:   "gw.cds.amcn.com",
+         Path:   "/auth-orchestration-id/api/v1/unauth",
+      },
+      map[string]string{
+         "x-amcn-network":   "amcplus",
+         "x-amcn-platform":  "web",
+         "x-amcn-tenant":    "amcn",
+         "x-amcn-device-id": "-",
+         "x-amcn-language":  "en",
+      },
+      nil,
+   )
    if err != nil {
       return nil, err
    }
    defer resp.Body.Close()
+   if resp.StatusCode != 200 {
+      return nil, fmt.Errorf("unauth failed with status: %d", resp.StatusCode)
+   }
+   // Internal envelope to strip the first layer
+   var envelope struct {
+      Success bool     `json:"success"`
+      Status  int      `json:"status"`
+      Data    AuthData `json:"data"`
+   }
+   if err := json.NewDecoder(resp.Body).Decode(&envelope); err != nil {
+      return nil, err
+   }
+   return &envelope.Data, nil
+}
 
-   if resp.StatusCode != http.StatusOK {
+func SeriesDetail(authToken string, seriesId int) (*ContentNode, error) {
+   resp, err := maya.Get(
+      &url.URL{
+         Scheme: "https",
+         Host:   "gw.cds.amcn.com",
+         Path: fmt.Sprint(
+            "/content-compiler-cr/api/v1/content/amcn/amcplus/type/series-detail/id/",
+            seriesId,
+         ),
+      },
+      map[string]string{
+         "authorization":   "Bearer " + authToken,
+         "x-amcn-network":  "amcplus",
+         "x-amcn-platform": "android",
+         "x-amcn-tenant":   "amcn",
+      },
+   )
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   if resp.StatusCode != 200 {
+      return nil, fmt.Errorf("series detail failed with status: %d", resp.StatusCode)
+   }
+   // Internal envelope to strip the first layer
+   var envelope struct {
+      Success bool        `json:"success"`
+      Status  int         `json:"status"`
+      Data    ContentNode `json:"data"`
+   }
+   if err := json.NewDecoder(resp.Body).Decode(&envelope); err != nil {
+      return nil, err
+   }
+   return &envelope.Data, nil
+}
+
+//go:embed playback.json
+var playback_json []byte
+
+func Playback(authToken string, videoId int) (*PlaybackResult, error) {
+   resp, err := maya.Post(
+      &url.URL{
+         Scheme: "https",
+         Host:   "gw.cds.amcn.com",
+         Path:   fmt.Sprint("/playback-id/api/v1/playback/", videoId),
+      },
+      map[string]string{
+         "authorization":       "Bearer " + authToken,
+         "content-type":        "application/json",
+         "x-amcn-language":     "en",
+         "x-amcn-network":      "amcplus",
+         "x-amcn-platform":     "web",
+         "x-amcn-service-id":   "amcplus",
+         "x-amcn-tenant":       "amcn",
+         "x-amcn-device-ad-id": "-",
+         "x-ccpa-do-not-sell":  "doNotPassData",
+      },
+      playback_json,
+   )
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   if resp.StatusCode != 200 {
       return nil, fmt.Errorf("playback failed with status: %d", resp.StatusCode)
    }
-
    // Internal envelope to strip the first layer
    var envelope struct {
       Success bool         `json:"success"`
       Status  int          `json:"status"`
       Data    PlaybackData `json:"data"`
    }
-
    if err := json.NewDecoder(resp.Body).Decode(&envelope); err != nil {
       return nil, err
    }
-
    return &PlaybackResult{
       Data:     envelope.Data,
       BcovAuth: resp.Header.Get("x-amcn-bc-jwt"),
