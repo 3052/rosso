282fb9265a10c86a135d150d41a92dc950d6ce1d
27 <27@riseup.net>
2026-04-18 13:21:57 -0500

    +   resp, err := maya.Post(

--- roku/maya.go
+++ roku/maya.go
@@ -1,20 +1,28 @@
 package roku
 
 import (
+   "41.neocities.org/maya"
    "bytes"
    "encoding/json"
+   "errors"
+   "io"
    "net/http"
+   "net/url"
 )
 
-// /api/v1/account/activation
-func FetchActivation(authToken string) (*Activation, error) {
-   body, err := json.Marshal(map[string]string{"platform": "googletv"})
+// /api/v3/playback
+func FetchPlayback(authToken, rokuId string) (*Playback, error) {
+   data, err := json.Marshal(map[string]string{
+      "mediaFormat": "DASH",
+      "providerId":  "rokuavod",
+      "rokuId":      rokuId,
+   })
    if err != nil {
       return nil, err
    }
    req, err := http.NewRequest(
-      "POST", "https://googletv.web.roku.com/api/v1/account/activation",
-      bytes.NewReader(body),
+      "POST", "https://googletv.web.roku.com/api/v3/playback",
+      bytes.NewReader(data),
    )
    if err != nil {
       return nil, err
@@ -23,6 +31,107 @@ func FetchActivation(authToken string) (*Activation, error) {
    req.Header.Set("user-agent", user_agent)
    req.Header.Set("x-roku-content-token", authToken)
    resp, err := http.DefaultClient.Do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   if resp.StatusCode != http.StatusOK {
+      return nil, errors.New(resp.Status)
+   }
+   result := &Playback{}
+   err = json.NewDecoder(resp.Body).Decode(result)
+   if err != nil {
+      return nil, err
+   }
+   return result, nil
+}
+
+func FetchWidevine(licenseServer string, body []byte) ([]byte, error) {
+   target, err := url.Parse(licenseServer)
+   if err != nil {
+      return nil, err
+   }
+   resp, err := maya.Post(
+      target, map[string]string{"content-type": "application/x-protobuf"}, body,
+   )
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   if resp.StatusCode != 200 {
+      return nil, errors.New(resp.Status)
+   }
+   return io.ReadAll(resp.Body)
+}
+
+// codeToken can be empty
+//
+// /api/v1/account/token
+func FetchToken(codeToken string) (*Token, error) {
+   header := map[string]string{"user-agent": user_agent}
+   if codeToken != "" {
+      header["x-roku-content-token"] = codeToken
+   }
+   resp, err := maya.Get(
+      &url.URL{
+         Scheme: "https",
+         Host:   "googletv.web.roku.com",
+         Path:   "/api/v1/account/token",
+      },
+      header,
+   )
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   result := &Token{}
+   err = json.NewDecoder(resp.Body).Decode(result)
+   if err != nil {
+      return nil, err
+   }
+   return result, nil
+}
+
+// /api/v1/account/activation/code
+func FetchCode(authToken, activationCode string) (*Code, error) {
+   resp, err := maya.Get(
+      &url.URL{
+         Scheme: "https",
+         Host:   "googletv.web.roku.com",
+         Path:   "/api/v1/account/activation/" + activationCode,
+      },
+      map[string]string{
+         "user-agent":           user_agent,
+         "x-roku-content-token": authToken,
+      },
+   )
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   result := &Code{}
+   err = json.NewDecoder(resp.Body).Decode(result)
+   if err != nil {
+      return nil, err
+   }
+   return result, nil
+}
+
+// /api/v1/account/activation
+func FetchActivation(authToken string) (*Activation, error) {
+   resp, err := maya.Post(
+      &url.URL{
+         Scheme: "https",
+         Host:   "googletv.web.roku.com",
+         Path:   "/api/v1/account/activation",
+      },
+      map[string]string{
+         "content-type":         "application/json",
+         "user-agent":           user_agent,
+         "x-roku-content-token": authToken,
+      },
+      []byte(`{"platform": "googletv"}`),
+   )
    if err != nil {
       return nil, err
    }
