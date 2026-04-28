599ccfef4704609a8e63226f2cb1361df10706c4
27 <27@riseup.net>
2026-04-27 21:37:10 -0500

    deleted:    rakuten/fetch_movie.go

--- rakuten/rakuten.go
+++ rakuten/rakuten.go
@@ -11,6 +11,141 @@ import (
    "strings"
 )
 
+type StartResponse struct {
+   Profile Profile `json:"profile"`
+   Market  Market  `json:"market"`
+}
+
+type Profile struct {
+   Classification Classification `json:"classification"`
+   AudioLanguage  Language       `json:"audio_language"`
+}
+
+type Classification struct {
+   NumericalId int `json:"numerical_id"`
+}
+
+type Language struct {
+   Id string `json:"id"`
+}
+
+type Market struct {
+   Code string `json:"code"`
+}
+
+type StartRequest struct {
+   DeviceIdentifier string         `json:"device_identifier"`
+   DeviceMetadata   DeviceMetadata `json:"device_metadata"`
+}
+
+type DeviceMetadata struct {
+   AppVersion   string `json:"app_version"`
+   Brand        string `json:"brand"`
+   Model        string `json:"model"`
+   Os           string `json:"os"`
+   SerialNumber string `json:"serial_number"`
+   Uid          string `json:"uid"`
+   Year         int    `json:"year"`
+}
+
+func FetchStart(marketCode string) (*StartResponse, error) {
+   target := &url.URL{
+      Scheme: "https",
+      Host:   "gizmo.rakuten.tv",
+      Path:   "/v3/me/start",
+   }
+
+   query := url.Values{}
+   query.Set("market_code", marketCode)
+   target.RawQuery = query.Encode()
+
+   payload := StartRequest{
+      DeviceIdentifier: "web",
+      DeviceMetadata: DeviceMetadata{
+         AppVersion:   "app_version",
+         Brand:        "brand",
+         Model:        "model",
+         Os:           "os",
+         SerialNumber: "serial_number",
+         Uid:          "uid",
+         Year:         0,
+      },
+   }
+
+   reqBytes, err := json.Marshal(payload)
+   if err != nil {
+      return nil, err
+   }
+
+   headers := map[string]string{
+      "content-type": "application/json",
+   }
+
+   resp, err := maya.Post(target, headers, reqBytes)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+
+   var respWrapper struct {
+      Data StartResponse `json:"data"`
+   }
+
+   if err := json.NewDecoder(resp.Body).Decode(&respWrapper); err != nil {
+      return nil, err
+   }
+
+   return &respWrapper.Data, nil
+}
+
+type Movie struct {
+   Id          string      `json:"id"`
+   Title       string      `json:"title"`
+   ViewOptions ViewOptions `json:"view_options"`
+}
+
+type ViewOptions struct {
+   Private Private `json:"private"`
+}
+
+type Private struct {
+   Streams []Stream `json:"streams"`
+}
+
+type Stream struct {
+   AudioLanguages []Language `json:"audio_languages"`
+}
+
+func FetchMovie(movieId string, rating *Classification, region *Market) (*Movie, error) {
+   target := &url.URL{
+      Scheme: "https",
+      Host:   "gizmo.rakuten.tv",
+      Path:   "/v3/movies/" + movieId,
+   }
+
+   query := url.Values{}
+   query.Set("classification_id", strconv.Itoa(rating.NumericalId))
+   query.Set("device_identifier", "atvui40")
+   query.Set("market_code", region.Code)
+   target.RawQuery = query.Encode()
+
+   resp, err := maya.Get(target, nil)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+
+   var respWrapper struct {
+      Data Movie `json:"data"`
+   }
+
+   if err := json.NewDecoder(resp.Body).Decode(&respWrapper); err != nil {
+      return nil, err
+   }
+
+   return &respWrapper.Data, nil
+}
+
 type SeasonDetails struct {
    Id       string    `json:"id"`
    Title    string    `json:"title"`
