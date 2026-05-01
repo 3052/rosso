443a251da04c0eb200ad3257019862b3be31f671
27 <27@riseup.net>
2026-05-01 18:11:16 -0500

    +func (a *Account) Login(profileId string) error {

--- crave/crave.go
+++ crave/crave.go
@@ -13,6 +13,81 @@ import (
    "strings"
 )
 
+// 699710369328da351ac33c63
+func (a *Account) Login(profileId string) error {
+   body := url.Values{
+      "grant_type":    {"refresh_token"},
+      "profile_id":    {profileId},
+      "refresh_token": {a.RefreshToken},
+   }.Encode()
+   resp, err := maya.Post(
+      &url.URL{
+         Scheme: "https",
+         Host:   "account.bellmedia.ca",
+         Path:   "/api/login/v2.2",
+      },
+      map[string]string{
+         "authorization": crave_web,
+         "content-type":  "application/x-www-form-urlencoded",
+      },
+      []byte(body),
+   )
+   if err != nil {
+      return err
+   }
+   defer resp.Body.Close()
+   if resp.StatusCode != 200 {
+      return fmt.Errorf("profile login failed with: %v", resp.Status)
+   }
+   return json.NewDecoder(resp.Body).Decode(a)
+}
+
+func (a *Account) FetchProfiles() ([]*Profile, error) {
+   resp, err := maya.Get(
+      &url.URL{
+         Scheme: "https",
+         Host:   "account.bellmedia.ca",
+         Path:   "/api/profile/v2/account/" + a.AccountId,
+      },
+      map[string]string{"authorization": "Bearer " + a.AccessToken},
+   )
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   if resp.StatusCode != 200 {
+      return nil, fmt.Errorf("failed to fetch profiles with: %v", resp.Status)
+   }
+   var profiles []*Profile
+   if err := json.NewDecoder(resp.Body).Decode(&profiles); err != nil {
+      return nil, err
+   }
+   return profiles, nil
+}
+
+func FetchSubscriptions(accessToken string) ([]Subscription, error) {
+   resp, err := maya.Get(
+      &url.URL{
+         Scheme: "https",
+         Host:   "account.bellmedia.ca",
+         Path:   "/api/subscription/v5",
+      },
+      map[string]string{"authorization": "Bearer " + accessToken},
+   )
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+   var result struct {
+      Subscriptions []Subscription
+   }
+   err = json.NewDecoder(resp.Body).Decode(&result)
+   if err != nil {
+      return nil, err
+   }
+   return result.Subscriptions, nil
+}
+
 func (c *ContentPackage) fetchManifest(contentId int, accessToken string, platformId int) (*Manifest, error) {
    resp, err := maya.Get(
       &url.URL{
@@ -239,81 +314,6 @@ func ParseMedia(rawUrl string) (*Media, error) {
       media_data.FirstContent.Id = id
    }
    return media_data, nil
-}
-
-// 699710369328da351ac33c63
-func (a *Account) Login(profileId string) error {
-   body := url.Values{
-      "grant_type":    {"refresh_token"},
-      "profile_id":    {profileId},
-      "refresh_token": {a.RefreshToken},
-   }.Encode()
-   resp, err := maya.Post(
-      &url.URL{
-         Scheme: "https",
-         Host:   "account.bellmedia.ca",
-         Path:   "/api/login/v2.2",
-      },
-      map[string]string{
-         "authorization": crave_web,
-         "content-type":  "application/x-www-form-urlencoded",
-      },
-      []byte(body),
-   )
-   if err != nil {
-      return err
-   }
-   defer resp.Body.Close()
-   if resp.StatusCode != 200 {
-      return fmt.Errorf("profile login failed with: %v", resp.Status)
-   }
-   return json.NewDecoder(resp.Body).Decode(a)
-}
-
-func (a *Account) FetchProfiles() ([]*Profile, error) {
-   resp, err := maya.Get(
-      &url.URL{
-         Scheme: "https",
-         Host:   "account.bellmedia.ca",
-         Path:   "/api/profile/v2/account/" + a.AccountId,
-      },
-      map[string]string{"authorization": "Bearer " + a.AccessToken},
-   )
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-   if resp.StatusCode != 200 {
-      return nil, fmt.Errorf("failed to fetch profiles with: %v", resp.Status)
-   }
-   var profiles []*Profile
-   if err := json.NewDecoder(resp.Body).Decode(&profiles); err != nil {
-      return nil, err
-   }
-   return profiles, nil
-}
-
-func FetchSubscriptions(accessToken string) ([]Subscription, error) {
-   resp, err := maya.Get(
-      &url.URL{
-         Scheme: "https",
-         Host:   "account.bellmedia.ca",
-         Path:   "/api/subscription/v5",
-      },
-      map[string]string{"authorization": "Bearer " + accessToken},
-   )
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-   var result struct {
-      Subscriptions []Subscription
-   }
-   err = json.NewDecoder(resp.Body).Decode(&result)
-   if err != nil {
-      return nil, err
-   }
-   return result.Subscriptions, nil
 }
 
 func (m *Manifest) GetManifest() (*url.URL, error) {
