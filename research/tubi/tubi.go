444e83ef63f39e60ecac7d2586a14bd315a52d97
27 <27@riseup.net>
2026-05-05 21:57:01 -0500

    new file:   tubi/content.go

--- research/tubi/tubi.go
+++ research/tubi/tubi.go
@@ -2,9 +2,16 @@ package main
 
 import (
    "41.neocities.org/maya"
+   "41.neocities.org/rosso/research/user"
    "41.neocities.org/rosso/tubi"
    "log"
 )
+
+type client struct {
+   cache   user.Cache
+   job     maya.Job
+   tubi_id int
+}
 
 func main() {
    log.SetFlags(log.Ltime)
@@ -14,52 +21,23 @@ func main() {
    }
 }
 
-func (c *client) do() error {
-   if err := c.cache.Setup("rosso/tubi"); err != nil {
-      return err
-   }
-   c.cache.Decode(&c.Job)
-   widevine := maya.StringFlag(&c.Job.Widevine, "w", "Widevine")
-   tubi_id := maya.IntFlag(&c.tubi_id, "t", "Tubi ID")
-   dash := maya.StringFlag(&c.Job.Dash, "d", "DASH ID")
-   if err := maya.ParseFlags(); err != nil {
-      return err
-   }
-   switch {
-   case widevine.IsSet:
-      return c.cache.Encode(c.Job)
-   case tubi_id.IsSet:
-      return c.do_tubi()
-   case dash.IsSet:
-      return c.do_dash()
-   }
-   return maya.PrintFlags([][]*maya.Flag{{
-      widevine,
-      tubi_id,
-      dash,
-   }})
-}
-
 func (c *client) do_tubi() error {
    content, err := tubi.GetContent(c.tubi_id)
    if err != nil {
       return err
    }
+
    video := content.VideoResources[0]
    if err := c.cache.Encode(video.LicenseServer); err != nil {
       return err
    }
+
    dash, err := maya.ListDash(video.GetManifest)
    if err != nil {
       return err
    }
-   return c.cache.Encode(dash)
-}
 
-type client struct {
-   Job     maya.Job
-   cache   Cache
-   tubi_id int
+   return c.cache.Encode(dash)
 }
 
 func (c *client) do_dash() error {
@@ -67,11 +45,43 @@ func (c *client) do_dash() error {
    if err := c.cache.Decode(&dash); err != nil {
       return err
    }
+
    var server tubi.LicenseServer
    if err := c.cache.Decode(&server); err != nil {
       return err
    }
-   return dash.Download(&c.Job, func(body []byte) ([]byte, error) {
+
+   return dash.Download(&c.job, func(body []byte) ([]byte, error) {
       return tubi.AcquireLicense(&server, body)
    })
+}
+
+func (c *client) do() error {
+   if err := c.cache.Setup("rosso/tubi"); err != nil {
+      return err
+   }
+   c.cache.Decode(&c.job)
+
+   widevine := maya.StringFlag(&c.job.Widevine, "w", "Widevine")
+   tubi_id := maya.IntFlag(&c.tubi_id, "t", "Tubi ID")
+   dash := maya.StringFlag(&c.job.Dash, "d", "DASH ID")
+
+   if err := maya.ParseFlags(); err != nil {
+      return err
+   }
+
+   switch {
+   case widevine.IsSet:
+      return c.cache.Encode(c.job)
+   case tubi_id.IsSet:
+      return c.do_tubi()
+   case dash.IsSet:
+      return c.do_dash()
+   }
+
+   return maya.PrintFlags([][]*maya.Flag{{
+      widevine,
+      tubi_id,
+      dash,
+   }})
 }
