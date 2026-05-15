cd9eb49f38d64b09d8b45432e45faac71ac0f4f2
27 <27@riseup.net>
2026-05-14 22:31:00 -0500

    +   dash     maya.Flag

--- examples/tubi/tubi.go
+++ examples/tubi/tubi.go
@@ -3,62 +3,38 @@ package main
 import (
    "41.neocities.org/maya"
    "41.neocities.org/rosso/tubi"
+   "fmt"
    "log"
 )
 
 type client struct {
    cache    maya.Cache
-   dash     string
    flag     maya.FlagSet
-   tubi_id  int
-   widevine string
+   dash     maya.Flag
+   tubi_id  maya.Flag
+   widevine maya.Flag
 }
 
 func (c *client) do() error {
    if err := c.cache.Setup("rosso/tubi"); err != nil {
       return err
    }
-   tubi_id := c.flag.Int(&c.tubi_id, "t", "Tubi ID")
-   dash := c.flag.String(&c.dash, "d", "DASH ID")
-   widevine := c.flag.String(&c.widevine, "w", "Widevine folder")
+   c.flag.AddValue(&c.widevine, "w", "Widevine")
+   c.flag.AddValue(&c.tubi_id, "t", "Tubi ID")
+   c.flag.AddValue(&c.dash, "d", "DASH ID")
    if err := c.flag.Parse(); err != nil {
       return err
    }
    switch {
-   case widevine.IsSet:
-      return c.cache.Encode(widevine_device(c.widevine))
-   case tubi_id.IsSet:
+   case c.widevine.Set:
+      return c.cache.Encode(widevine_device(c.widevine.Value))
+   case c.tubi_id.Set:
       return c.do_tubi()
-   case dash.IsSet:
+   case c.dash.Set:
       return c.do_dash()
    }
-   return maya.PrintFlags([]maya.FlagSet{{
-      widevine,
-      tubi_id,
-      dash,
-   }})
-}
-
-type widevine_device string
-
-func (c *client) do_dash() error {
-   var (
-      device   widevine_device
-      manifest maya.Manifest
-      server   tubi.LicenseServer
-   )
-   err := c.cache.Decode(&device, &manifest, &server)
-   if err != nil {
-      return err
-   }
-   license := func(body []byte) ([]byte, error) {
-      return tubi.AcquireLicense(&server, body)
-   }
-   return maya.DownloadDash(c.dash.Value, &manifest, &maya.Options{
-      Device:  string(device),
-      Drm:     maya.DrmWidevine,
-      License: license,
-   })
+   fmt.Println(c.flag)
+   return nil
 }
 
 func (c *client) do_tubi() error {
@@ -84,4 +60,26 @@ func main() {
    if err != nil {
       log.Fatal(err)
    }
+}
+
+type widevine_device string
+
+func (c *client) do_dash() error {
+   var (
+      device   widevine_device
+      manifest maya.Manifest
+      server   tubi.LicenseServer
+   )
+   err := c.cache.Decode(&device, &manifest, &server)
+   if err != nil {
+      return err
+   }
+   license := func(body []byte) ([]byte, error) {
+      return tubi.AcquireLicense(&server, body)
+   }
+   return maya.DownloadDash(c.dash.Value, &manifest, &maya.Options{
+      Device:  string(device),
+      Drm:     maya.DrmWidevine,
+      License: license,
+   })
 }
