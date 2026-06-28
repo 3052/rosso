97528c4d469c06d9b419c7aa172152f3985fa6a4
27 <27@riseup.net>
2026-06-28 08:21:24 -0500

    +// need to relog. also if you get a failed license request try provision again

--- amazon/client.go
+++ amazon/client.go
@@ -15,7 +15,7 @@ const ( // API Hosts
 const DeviceID = "deviceID"
 
 // the wrong DTID will fail the license request. if you change the DTID you
-// need to relog
+// need to relog. also if you get a failed license request try provision again
 var Devices = []Device{
    {
       Manufacturer:  "Hisense",
--- rakuten/rakuten.go
+++ rakuten/rakuten.go
@@ -11,32 +11,25 @@ import (
    "strings"
 )
 
-func (*StreamInfo) CachePath() string {
-   return "rosso/rakuten/StreamInfo"
-}
-
-type StreamInfo struct {
-   LicenseUrl *Url `json:"license_url"`
-   Url        *Url // MPD
-}
-
-func (*Address) CachePath() string {
-   return "rosso/rakuten/Address"
+func formatPlayableDetails(identifier string, title string, playbackStreams []Stream) string {
+   seenLanguages := make(map[string]bool)
+   var availableLanguages []string
+   for _, currentStream := range playbackStreams {
+      for _, audioLanguage := range currentStream.AudioLanguages {
+         if !seenLanguages[audioLanguage.Id] {
+            seenLanguages[audioLanguage.Id] = true
+            availableLanguages = append(availableLanguages, audioLanguage.Id)
+         }
+      }
+   }
+   formattedAudio := strings.Join(availableLanguages, ", ")
+   return fmt.Sprintf("%s (%s) - Audio: %s", title, identifier, formattedAudio)
 }
 
 type Address struct {
    MarketCode  string
    ContentType string
    ContentId   string
-}
-
-func (*Start) CachePath() string {
-   return "rosso/rakuten/Start"
-}
-
-type Start struct {
-   Profile Profile
-   Market  Market
 }
 
 func ParseAddress(targetUrl string) (*Address, error) {
@@ -94,6 +87,10 @@ func ParseAddress(targetUrl string) (*Address, error) {
    return parsed, nil
 }
 
+func (*Address) CachePath() string {
+   return "rosso/rakuten/Address"
+}
+
 func (a *Address) IsMovie() bool {
    return a.ContentType == "movies"
 }
@@ -102,16 +99,91 @@ func (a *Address) IsTvShow() bool {
    return a.ContentType == "tv_shows"
 }
 
-type Season struct {
-   Id       string    `json:"id"`
-   Title    string    `json:"title"`
-   Episodes []Episode `json:"episodes"`
+type Classification struct {
+   NumericalId int `json:"numerical_id"`
+}
+
+type DeviceMetadata struct {
+   AppVersion   string `json:"app_version"`
+   Brand        string `json:"brand"`
+   Model        string `json:"model"`
+   Os           string `json:"os"`
+   SerialNumber string `json:"serial_number"`
+   Uid          string `json:"uid"`
+   Year         int    `json:"year"`
 }
 
 type Episode struct {
    Id          string      `json:"id"`
    Title       string      `json:"title"`
    ViewOptions ViewOptions `json:"view_options"`
+}
+
+func (e *Episode) String() string {
+   return formatPlayableDetails(e.Id, e.Title, e.ViewOptions.Private.Streams)
+}
+
+type Language struct {
+   Id string `json:"id"`
+}
+
+type Market struct {
+   Code string `json:"code"`
+}
+
+type Movie struct {
+   Id          string      `json:"id"`
+   Title       string      `json:"title"`
+   ViewOptions ViewOptions `json:"view_options"`
+}
+
+func FetchMovie(movieId string, userClassification Classification, targetMarket Market) (*Movie, error) {
+   target := &url.URL{
+      Scheme: "https",
+      Host:   "gizmo.rakuten.tv",
+      Path:   "/v3/movies/" + movieId,
+   }
+
+   query := url.Values{}
+   query.Set("classification_id", strconv.Itoa(userClassification.NumericalId))
+   query.Set("device_identifier", "atvui40")
+   query.Set("market_code", targetMarket.Code)
+   target.RawQuery = query.Encode()
+
+   resp, err := maya.Get(target, nil)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+
+   var apiResp struct {
+      Data Movie `json:"data"`
+   }
+
+   if err := json.NewDecoder(resp.Body).Decode(&apiResp); err != nil {
+      return nil, err
+   }
+
+   return &apiResp.Data, nil
+}
+
+func (targetMovie *Movie) String() string {
+   return formatPlayableDetails(targetMovie.Id, targetMovie.Title, targetMovie.ViewOptions.Private.Streams)
+}
+
+type Private struct {
+   Streams []Stream `json:"streams"`
+}
+
+type Profile struct {
+   Classification Classification `json:"classification"`
+   AudioLanguage  Language       `json:"audio_language"`
+}
+
+type Season struct {
+   Id       string    `json:"id"`
+   Title    string    `json:"title"`
+   Episodes []Episode `json:"episodes"`
 }
 
 func FetchSeason(id string, userClassification Classification, targetMarket Market) (*Season, error) {
@@ -144,37 +216,52 @@ func FetchSeason(id string, userClassification Classification, targetMarket Mark
    return &apiResp.Data, nil
 }
 
-func (e *Episode) String() string {
-   return formatPlayableDetails(e.Id, e.Title, e.ViewOptions.Private.Streams)
+type Start struct {
+   Profile Profile
+   Market  Market
 }
 
-type TvShow struct {
-   Id      string   `json:"id"`
-   Title   string   `json:"title"`
-   Seasons []Season `json:"seasons"`
-}
-
-func FetchTvShow(tvShowId string, userClassification Classification, targetMarket Market) (*TvShow, error) {
+func FetchStart(marketCode string) (*Start, error) {
    target := &url.URL{
       Scheme: "https",
       Host:   "gizmo.rakuten.tv",
-      Path:   "/v3/tv_shows/" + tvShowId,
+      Path:   "/v3/me/start",
    }
 
    query := url.Values{}
-   query.Set("classification_id", strconv.Itoa(userClassification.NumericalId))
-   query.Set("device_identifier", "atvui40")
-   query.Set("market_code", targetMarket.Code)
+   query.Set("market_code", marketCode)
    target.RawQuery = query.Encode()
 
-   resp, err := maya.Get(target, nil)
+   payload := StartRequest{
+      DeviceIdentifier: "web",
+      DeviceMetadata: DeviceMetadata{
+         AppVersion:   "app_version",
+         Brand:        "brand",
+         Model:        "model",
+         Os:           "os",
+         SerialNumber: "serial_number",
+         Uid:          "uid",
+         Year:         0,
+      },
+   }
+
+   body, err := json.Marshal(payload)
+   if err != nil {
+      return nil, err
+   }
+
+   headers := map[string]string{
+      "content-type": "application/json",
+   }
+
+   resp, err := maya.Post(target, headers, body)
    if err != nil {
       return nil, err
    }
    defer resp.Body.Close()
 
    var apiResp struct {
-      Data TvShow `json:"data"`
+      Data Start `json:"data"`
    }
 
    if err := json.NewDecoder(resp.Body).Decode(&apiResp); err != nil {
@@ -184,38 +271,30 @@ func FetchTvShow(tvShowId string, userClassification Classification, targetMarke
    return &apiResp.Data, nil
 }
 
-func (show *TvShow) String() string {
-   var data strings.Builder
-   for i, currentSeason := range show.Seasons {
-      if i >= 1 {
-         data.WriteByte('\n')
-      }
-      data.WriteString("season id: ")
-      data.WriteString(currentSeason.Id)
-   }
-   return data.String()
+func (*Start) CachePath() string {
+   return "rosso/rakuten/Start"
 }
 
-type StreamingRequest struct {
-   AudioLanguage            string `json:"audio_language"`
-   AudioQuality             string `json:"audio_quality"`
-   ClassificationId         int    `json:"classification_id"`
-   ContentId                string `json:"content_id"`
-   ContentType              string `json:"content_type"`
-   DeviceIdentifier         string `json:"device_identifier"`
-   DeviceSerial             string `json:"device_serial"`
-   DeviceStreamVideoQuality string `json:"device_stream_video_quality"`
-   Player                   string `json:"player"`
-   SubtitleLanguage         string `json:"subtitle_language"`
-   VideoType                string `json:"video_type"`
+type StartRequest struct {
+   DeviceIdentifier string         `json:"device_identifier"`
+   DeviceMetadata   DeviceMetadata `json:"device_metadata"`
 }
 
-func FetchMovieStreaming(contentId string, userClassification Classification, audioLanguage string) (*StreamInfo, error) {
-   return fetchStreaming(contentId, "movies", userClassification, audioLanguage)
+type Stream struct {
+   AudioLanguages []Language `json:"audio_languages"`
+}
+
+type StreamInfo struct {
+   LicenseUrl *Url `json:"license_url"`
+   Url        *Url // MPD
 }
 
 func FetchEpisodeStreaming(contentId string, userClassification Classification, audioLanguage string) (*StreamInfo, error) {
    return fetchStreaming(contentId, "episodes", userClassification, audioLanguage)
+}
+
+func FetchMovieStreaming(contentId string, userClassification Classification, audioLanguage string) (*StreamInfo, error) {
+   return fetchStreaming(contentId, "movies", userClassification, audioLanguage)
 }
 
 func fetchStreaming(contentId string, contentType string, userClassification Classification, audioLanguage string) (*StreamInfo, error) {
@@ -271,6 +350,10 @@ func fetchStreaming(contentId string, contentType string, userClassification Cla
    return nil, errors.New("no stream infos found")
 }
 
+func (*StreamInfo) CachePath() string {
+   return "rosso/rakuten/StreamInfo"
+}
+
 func (s *StreamInfo) FetchLicense(challenge []byte) ([]byte, error) {
    resp, err := maya.Post(&s.LicenseUrl.Url, nil, challenge)
    if err != nil {
@@ -280,123 +363,31 @@ func (s *StreamInfo) FetchLicense(challenge []byte) ([]byte, error) {
    return io.ReadAll(resp.Body)
 }
 
-type Url struct {
-   Url url.URL
+type StreamingRequest struct {
+   AudioLanguage            string `json:"audio_language"`
+   AudioQuality             string `json:"audio_quality"`
+   ClassificationId         int    `json:"classification_id"`
+   ContentId                string `json:"content_id"`
+   ContentType              string `json:"content_type"`
+   DeviceIdentifier         string `json:"device_identifier"`
+   DeviceSerial             string `json:"device_serial"`
+   DeviceStreamVideoQuality string `json:"device_stream_video_quality"`
+   Player                   string `json:"player"`
+   SubtitleLanguage         string `json:"subtitle_language"`
+   VideoType                string `json:"video_type"`
 }
 
-func (u *Url) UnmarshalText(text []byte) error {
-   return u.Url.UnmarshalBinary(text)
+type TvShow struct {
+   Id      string   `json:"id"`
+   Title   string   `json:"title"`
+   Seasons []Season `json:"seasons"`
 }
 
-func (u *Url) MarshalText() ([]byte, error) {
-   return u.Url.MarshalBinary()
-}
-
-type Profile struct {
-   Classification Classification `json:"classification"`
-   AudioLanguage  Language       `json:"audio_language"`
-}
-
-type Classification struct {
-   NumericalId int `json:"numerical_id"`
-}
-
-type Language struct {
-   Id string `json:"id"`
-}
-
-type Market struct {
-   Code string `json:"code"`
-}
-
-type StartRequest struct {
-   DeviceIdentifier string         `json:"device_identifier"`
-   DeviceMetadata   DeviceMetadata `json:"device_metadata"`
-}
-
-type DeviceMetadata struct {
-   AppVersion   string `json:"app_version"`
-   Brand        string `json:"brand"`
-   Model        string `json:"model"`
-   Os           string `json:"os"`
-   SerialNumber string `json:"serial_number"`
-   Uid          string `json:"uid"`
-   Year         int    `json:"year"`
-}
-
-func FetchStart(marketCode string) (*Start, error) {
+func FetchTvShow(tvShowId string, userClassification Classification, targetMarket Market) (*TvShow, error) {
    target := &url.URL{
       Scheme: "https",
       Host:   "gizmo.rakuten.tv",
-      Path:   "/v3/me/start",
-   }
-
-   query := url.Values{}
-   query.Set("market_code", marketCode)
-   target.RawQuery = query.Encode()
-
-   payload := StartRequest{
-      DeviceIdentifier: "web",
-      DeviceMetadata: DeviceMetadata{
-         AppVersion:   "app_version",
-         Brand:        "brand",
-         Model:        "model",
-         Os:           "os",
-         SerialNumber: "serial_number",
-         Uid:          "uid",
-         Year:         0,
-      },
-   }
-
-   body, err := json.Marshal(payload)
-   if err != nil {
-      return nil, err
-   }
-
-   headers := map[string]string{
-      "content-type": "application/json",
-   }
-
-   resp, err := maya.Post(target, headers, body)
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-
-   var apiResp struct {
-      Data Start `json:"data"`
-   }
-
-   if err := json.NewDecoder(resp.Body).Decode(&apiResp); err != nil {
-      return nil, err
-   }
-
-   return &apiResp.Data, nil
-}
-
-type Movie struct {
-   Id          string      `json:"id"`
-   Title       string      `json:"title"`
-   ViewOptions ViewOptions `json:"view_options"`
-}
-
-type ViewOptions struct {
-   Private Private `json:"private"`
-}
-
-type Private struct {
-   Streams []Stream `json:"streams"`
-}
-
-type Stream struct {
-   AudioLanguages []Language `json:"audio_languages"`
-}
-
-func FetchMovie(movieId string, userClassification Classification, targetMarket Market) (*Movie, error) {
-   target := &url.URL{
-      Scheme: "https",
-      Host:   "gizmo.rakuten.tv",
-      Path:   "/v3/movies/" + movieId,
+      Path:   "/v3/tv_shows/" + tvShowId,
    }
 
    query := url.Values{}
@@ -412,7 +403,7 @@ func FetchMovie(movieId string, userClassification Classification, targetMarket 
    defer resp.Body.Close()
 
    var apiResp struct {
-      Data Movie `json:"data"`
+      Data TvShow `json:"data"`
    }
 
    if err := json.NewDecoder(resp.Body).Decode(&apiResp); err != nil {
@@ -422,21 +413,30 @@ func FetchMovie(movieId string, userClassification Classification, targetMarket 
    return &apiResp.Data, nil
 }
 
-func (targetMovie *Movie) String() string {
-   return formatPlayableDetails(targetMovie.Id, targetMovie.Title, targetMovie.ViewOptions.Private.Streams)
+func (show *TvShow) String() string {
+   var data strings.Builder
+   for i, currentSeason := range show.Seasons {
+      if i >= 1 {
+         data.WriteByte('\n')
+      }
+      data.WriteString("season id: ")
+      data.WriteString(currentSeason.Id)
+   }
+   return data.String()
 }
 
-func formatPlayableDetails(identifier string, title string, playbackStreams []Stream) string {
-   seenLanguages := make(map[string]bool)
-   var availableLanguages []string
-   for _, currentStream := range playbackStreams {
-      for _, audioLanguage := range currentStream.AudioLanguages {
-         if !seenLanguages[audioLanguage.Id] {
-            seenLanguages[audioLanguage.Id] = true
-            availableLanguages = append(availableLanguages, audioLanguage.Id)
-         }
-      }
-   }
-   formattedAudio := strings.Join(availableLanguages, ", ")
-   return fmt.Sprintf("%s (%s) - Audio: %s", title, identifier, formattedAudio)
+type Url struct {
+   Url url.URL
+}
+
+func (u *Url) MarshalText() ([]byte, error) {
+   return u.Url.MarshalBinary()
+}
+
+func (u *Url) UnmarshalText(text []byte) error {
+   return u.Url.UnmarshalBinary(text)
+}
+
+type ViewOptions struct {
+   Private Private `json:"private"`
 }
