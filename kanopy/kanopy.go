f2a9ccfd13b63fcd2d50f95653203f9736b638cc
27 <27@riseup.net>
2026-08-17 17:29:12 -0500

    new file:   kanopy/plays.go

--- kanopy/kanopy.go
+++ kanopy/kanopy.go
@@ -5,7 +5,6 @@ import (
    "encoding/json"
    "errors"
    "io"
-   "log"
    "net/http"
    "net/url"
    "path"
@@ -32,32 +31,6 @@ func CreateLicense(loginData *Login, manifestData *Manifest, challenge []byte) (
    defer resp.Body.Close()
 
    return io.ReadAll(resp.Body)
-}
-
-func do(req *http.Request) (*http.Response, error) {
-   log.Println(req.Method, req.URL)
-   return http.DefaultClient.Do(req)
-}
-
-type Caption struct {
-   Language string `json:"language"`
-   Files    []File `json:"files"`
-   Label    string `json:"label"`
-}
-
-type File struct {
-   Type string `json:"type"`
-   Url  string
-}
-
-type Login struct {
-   Jwt               string `json:"jwt"`
-   VisitorId         string `json:"visitorId"`
-   UserId            int    `json:"userId"`
-   KanopyKidsEnabled bool   `json:"kanopyKidsEnabled"`
-   WebshopId         int    `json:"webshopId"`
-   WebshopCode       string `json:"webshopCode"`
-   UserRole          string `json:"userRole"`
 }
 
 func LoginUser(email string, password string) (*Login, error) {
@@ -90,35 +63,6 @@ func LoginUser(email string, password string) (*Login, error) {
    return &loginData, nil
 }
 
-func (*Login) CachePath() string {
-   return "rosso/kanopy/Login"
-}
-
-type Manifest struct {
-   Url            string
-   ManifestType   string `json:"manifestType"`
-   DrmType        string `json:"drmType"`
-   StorageService string `json:"storageService"`
-   Cdn            string `json:"cdn"`
-   DrmLicenseId   string `json:"drmLicenseID"`
-}
-
-func (*Manifest) CachePath() string {
-   return "rosso/kanopy/Manifest"
-}
-
-type Membership struct {
-   IdentityId         int    `json:"identityId"`
-   DomainId           int    `json:"domainId"`
-   UserId             int    `json:"userId"`
-   Status             string `json:"status"`
-   IsDefault          bool   `json:"isDefault"`
-   Sitename           string `json:"sitename"`
-   Subdomain          string `json:"subdomain"`
-   TicketsAvailable   int    `json:"ticketsAvailable"`
-   MaxTicketsPerMonth int    `json:"maxTicketsPerMonth"`
-}
-
 func GetMemberships(loginData *Login) ([]Membership, error) {
    endpoint := &url.URL{
       Scheme: "https",
@@ -149,66 +93,6 @@ func GetMemberships(loginData *Login) ([]Membership, error) {
       return nil, err
    }
    return result.List, nil
-}
-
-type PlayRequest struct {
-   DomainId int `json:"domainId"`
-   UserId   int `json:"userId"`
-   VideoId  int `json:"videoId"`
-}
-
-type PlayResponse struct {
-   Captions  []Caption
-   Manifests []Manifest
-   PlayId    string
-}
-
-func CreatePlay(loginData *Login, membershipData *Membership, videoData *Video) (*PlayResponse, error) {
-   body, err := json.Marshal(PlayRequest{
-      DomainId: membershipData.DomainId,
-      UserId:   loginData.UserId,
-      VideoId:  videoData.VideoId,
-   })
-   if err != nil {
-      return nil, err
-   }
-   req, err := http.NewRequest("POST", "https://www.kanopy.com/kapi/plays", bytes.NewReader(body))
-   if err != nil {
-      return nil, err
-   }
-   req.Header.Set("authorization", "Bearer "+loginData.Jwt)
-   req.Header.Set("content-type", "application/json")
-   req.Header.Set("x-version", "web/undefined/undefined/undefined")
-
-   resp, err := do(req)
-   if err != nil {
-      return nil, err
-   }
-   defer resp.Body.Close()
-
-   var play PlayResponse
-   if err := json.NewDecoder(resp.Body).Decode(&play); err != nil {
-      return nil, err
-   }
-
-   return &play, nil
-}
-
-func (p *PlayResponse) GetDash() (*Manifest, error) {
-   for _, manifest_data := range p.Manifests {
-      if manifest_data.ManifestType == "dash" {
-         return &manifest_data, nil
-      }
-   }
-   return nil, errors.New("dash manifest not found")
-}
-
-type Video struct {
-   VideoId         int    `json:"videoId"`
-   Title           string `json:"title"`
-   DescriptionHtml string `json:"descriptionHtml"`
-   DurationSeconds int    `json:"durationSeconds"`
-   Alias           string `json:"alias"`
 }
 
 func GetVideo(loginData *Login, alias string) (*Video, error) {
@@ -273,4 +157,21 @@ func ParseVideo(rawUrl string) (*Video, error) {
       video.Alias = slug
    }
    return video, nil
+}
+
+func (*Login) CachePath() string {
+   return "rosso/kanopy/Login"
+}
+
+func (*Manifest) CachePath() string {
+   return "rosso/kanopy/Manifest"
+}
+
+func (p *PlayResponse) GetDash() (*Manifest, error) {
+   for _, manifest_data := range p.Manifests {
+      if manifest_data.ManifestType == "dash" {
+         return &manifest_data, nil
+      }
+   }
+   return nil, errors.New("dash manifest not found")
 }
---
+++ kanopy/plays.go
@@ -0,0 +1,106 @@
+package kanopy
+
+import (
+   "bytes"
+   "encoding/json"
+   "log"
+   "net/http"
+)
+
+func do(req *http.Request) (*http.Response, error) {
+   log.Println(req.Method, req.URL)
+   return http.DefaultClient.Do(req)
+}
+
+type Caption struct {
+   Language string `json:"language"`
+   Files    []File `json:"files"`
+   Label    string `json:"label"`
+}
+
+type File struct {
+   Type string
+   Url  string
+}
+
+type Login struct {
+   Jwt               string `json:"jwt"`
+   VisitorId         string `json:"visitorId"`
+   UserId            int    `json:"userId"`
+   KanopyKidsEnabled bool   `json:"kanopyKidsEnabled"`
+   WebshopId         int    `json:"webshopId"`
+   WebshopCode       string `json:"webshopCode"`
+   UserRole          string `json:"userRole"`
+}
+
+type Manifest struct {
+   Url            string
+   ManifestType   string `json:"manifestType"`
+   DrmType        string `json:"drmType"`
+   StorageService string `json:"storageService"`
+   Cdn            string `json:"cdn"`
+   DrmLicenseId   string `json:"drmLicenseID"`
+}
+
+type Membership struct {
+   IdentityId         int    `json:"identityId"`
+   DomainId           int    `json:"domainId"`
+   UserId             int    `json:"userId"`
+   Status             string `json:"status"`
+   IsDefault          bool   `json:"isDefault"`
+   Sitename           string `json:"sitename"`
+   Subdomain          string `json:"subdomain"`
+   TicketsAvailable   int    `json:"ticketsAvailable"`
+   MaxTicketsPerMonth int    `json:"maxTicketsPerMonth"`
+}
+
+type PlayRequest struct {
+   DomainId int `json:"domainId"`
+   UserId   int `json:"userId"`
+   VideoId  int `json:"videoId"`
+}
+
+type PlayResponse struct {
+   Captions  []Caption
+   Manifests []Manifest
+   PlayId    string
+}
+
+func CreatePlay(loginData *Login, membershipData *Membership, videoData *Video) (*PlayResponse, error) {
+   body, err := json.Marshal(PlayRequest{
+      DomainId: membershipData.DomainId,
+      UserId:   loginData.UserId,
+      VideoId:  videoData.VideoId,
+   })
+   if err != nil {
+      return nil, err
+   }
+   req, err := http.NewRequest("POST", "https://www.kanopy.com/kapi/plays", bytes.NewReader(body))
+   if err != nil {
+      return nil, err
+   }
+   req.Header.Set("authorization", "Bearer "+loginData.Jwt)
+   req.Header.Set("content-type", "application/json")
+   req.Header.Set("x-version", "web/undefined/undefined/undefined")
+
+   resp, err := do(req)
+   if err != nil {
+      return nil, err
+   }
+   defer resp.Body.Close()
+
+   var play PlayResponse
+   if err := json.NewDecoder(resp.Body).Decode(&play); err != nil {
+      return nil, err
+   }
+
+   return &play, nil
+}
+
+type Video struct {
+   VideoId         int    `json:"videoId"`
+   Title           string `json:"title"`
+   DescriptionHtml string `json:"descriptionHtml"`
+   DurationSeconds int    `json:"durationSeconds"`
+   Alias           string `json:"alias"`
+}
